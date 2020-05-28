edf4bd0d9b82b25dbcd902335749c15ee2f50f58
Viktor Perov <viktor@cloudflare.com>
2020-05-28 12:31:15 +0100

    Revert "Remove all metrics from broker except errors and throttle time"
    This reverts commit b2cde1b085e67fe2099da401824cb0a352985f7e.
    
    I didn't see any effect of absense of the metrics on the results.
    Throttling wasn't aslo happening.

--- prometheus_broker_metrics.go
+++ prometheus_broker_metrics.go
@@ -19,7 +19,7 @@ type prometheusBrokerMetrics struct {
 	joinGroupDuration  *prometheus.HistogramVec
 	syncGroupDuration  *prometheus.HistogramVec
 	heartBeatDuration  *prometheus.HistogramVec
-	fetchThrottleTime  *prometheus.HistogramVec
+	fetchThrottleMs    *prometheus.HistogramVec
 	leaveGroupDuration *prometheus.HistogramVec
 }
 
@@ -33,22 +33,91 @@ func init() {
 		Help:      "Total number of received errors on send()",
 	}, []string{"request_type", "broker"})
 
-	fetchThrottleTime := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	reqSendDuration := prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Namespace: bNamespace,
 		Subsystem: bSubsystem,
-		Name:      "fetch_throttle_seconds",
+		Name:      "request_send_duration_seconds",
+		Help:      "Duration of write request into connection with send() method in seconds",
+		Buckets:   []float64{1, 2, 5, 10, 60, 120, 300, 600, 900, 1800},
+	}, []string{"request_type", "broker"})
+	reqReceiveDuration := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: bNamespace,
+		Subsystem: bSubsystem,
+		Name:      "request_receive_duration_seconds",
+		Help:      "Elapsed time of reading header (first 8b) and body from connection by send() in seconds",
+		Buckets:   []float64{1, 2, 5, 10, 60, 120, 300, 600, 900, 1800},
+	}, []string{"request_type", "broker"})
+	reqReceiveBytes := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: bNamespace,
+		Subsystem: bSubsystem,
+		Name:      "request_receive_bytes",
+		Help:      "Amount of bytes that was read from connection",
+		Buckets:   []float64{10, 100, 1000, 5000, 10000, 1e6, 1e7},
+	}, []string{"request_type", "broker"})
+
+	joinGroupDuration := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: bNamespace,
+		Subsystem: bSubsystem,
+		Name:      "join_group_duration_seconds",
+		Help:      "JoinGroup request duration in seconds",
+		Buckets:   []float64{1, 2, 5, 10, 60, 120, 300, 600, 900, 1800},
+	}, []string{"member", "group", "broker"})
+
+	syncGroupDuration := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: bNamespace,
+		Subsystem: bSubsystem,
+		Name:      "sync_group_duration_seconds",
+		Help:      "SyncGroup request duration in seconds",
+		Buckets:   []float64{1, 2, 5, 10, 60, 120, 300, 600, 900, 1800},
+	}, []string{"member", "group", "broker"})
+
+	heartBeatDuration := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: bNamespace,
+		Subsystem: bSubsystem,
+		Name:      "heart_beat_duration_seconds",
+		Help:      "HeartBeat request duration in seconds",
+		Buckets:   []float64{1, 2, 5, 10, 60, 120, 300, 600, 900, 1800},
+	}, []string{"member", "group", "broker"})
+
+	fetchThrottleMs := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: bNamespace,
+		Subsystem: bSubsystem,
+		Name:      "fetch_throttle_milliseconds",
 		Help:      "Broker Fetch() request duration in seconds",
 		Buckets:   []float64{1, 2, 5, 10, 60, 120, 300, 600, 900, 1800},
 	}, []string{"topic", "partition", "broker"})
 
+	leaveGroupDuration := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: bNamespace,
+		Subsystem: bSubsystem,
+		Name:      "leave_group_duration_seconds",
+		Help:      "LeaveGroup request duration in seconds",
+		Buckets:   []float64{1, 2, 5, 10, 60, 120, 300, 600, 900, 1800},
+	}, []string{"member", "group", "broker"})
+
 	prometheus.MustRegister(
 		errorsTotal,
-		fetchThrottleTime,
+		reqSendDuration,
+		reqReceiveDuration,
+		reqReceiveBytes,
+		joinGroupDuration,
+		syncGroupDuration,
+		heartBeatDuration,
+		fetchThrottleMs,
+		leaveGroupDuration,
 	)
 
 	bMetrics = &prometheusBrokerMetrics{
 		errorsTotal: errorsTotal,
 
-		fetchThrottleTime: fetchThrottleTime,
+		reqSendDuration:    reqSendDuration,
+		reqReceiveDuration: reqReceiveDuration,
+		reqReceiveBytes:    reqReceiveBytes,
+
+		joinGroupDuration:  joinGroupDuration,
+		syncGroupDuration:  syncGroupDuration,
+		heartBeatDuration:  heartBeatDuration,
+		fetchThrottleMs:    fetchThrottleMs,
+		leaveGroupDuration: leaveGroupDuration,
 	}
 }
